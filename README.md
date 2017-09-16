f7d2d48a904d80e8f0a85765f7f63cc5fefda884
Jesper Eskilson <jesper@eskilson.se>
2017-09-16 20:15:33 +0200

    Experiments

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ https://en.wikipedia.org/wiki/Mexican_Train.
 *** Maven artifacts by https://github.com/iominh/Marven
 
 * Counting beans example: http://marvinproject.sourceforge.net/en/examples/countingBeans.html
+* https://stackoverflow.com/questions/25789278/coffee-beans-separation-algorithm/25852298#25852298
+
+* http://opencv.org/
+
+* JavaCV
+
+  <dependency>
+    <groupId>org.bytedeco</groupId>
+    <artifactId>javacv-platform</artifactId>
+    <version>1.3.3</version>
+  </dependency>
--- mxt.core/src/test/java/mxt/core/MainTest.java
+++ mxt.core/src/test/java/mxt/core/MainTest.java
@@ -1,14 +1,39 @@
 package mxt.core;
 
-import java.io.InputStream;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.junit.Assert.*;
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+import org.bytedeco.javacv.*;
+import org.bytedeco.javacpp.*;
+import org.bytedeco.javacpp.indexer.*;
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_calib3d.*;
+import static org.bytedeco.javacpp.opencv_objdetect.*;
+import java.io.File;
 
+import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.junit.Test;
 
 public class MainTest {
 	@Test
 	public void testName() throws Exception {
-		try (InputStream str = getClass().getResource("/IMG_0648.JPG")
-				.openStream()) {
+		File basedir = new File(
+				"C:\\Users\\jespe\\OneDrive\\Documents\\GitHub\\mexican\\mxt.core");
+
+		File filename = new File(basedir, "src/test/resources/IMG_0648.JPG");
+
+		assertTrue(filename.exists());
+		IplImage image = cvLoadImage(filename.toString());
+		assertNotNull(image);
+
+		if (image != null) {
+			// cvSmooth(image, image);
+			// cvSaveImage(filename_smooth.toString(), image);
+			// cvReleaseImage(image);
 
 		}
 	}
